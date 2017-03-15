9009ac6ff1b7f0f20648880b080ad874cd3163fe
Oscar Espejo Sagués <xxgreysoulxx@gmail.com>
2017-03-15 17:26:12 +0100

    practica 1 empezada

--- Win32Project1/ArraysDynamics.cpp
+++ Win32Project1/ArraysDynamics.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
 #include <string>
 #include <ctime>
+#include <conio.h>
+
+int x;
+
+int selectordedificultad(int dificultad){
+	if (dificultad==1){
+		x = 1;
+		return x;
+	}
+	else if (dificultad == 2) {
+		x = 2;
+		return x;
+	}
+	else if (dificultad==3) {
+		x = 3;
+		return x;
+	}
+}
+
+class Player;
+class CoinManager;
 
 
-int *a;
-a = new int[10];
+class MAP {
+	char **md;
+	int row = 5*x;
+	int colum = 5*x*2;
 
+	md = new char *[row];
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < row; i++) {
+			md[i] = new char[colum];
+			md[i][j] = '.';
+		}
+	}
+
+};
 
 
