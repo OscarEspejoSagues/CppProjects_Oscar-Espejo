7c9151c05c02272b0c299142c1f7a300ce7d2d88
Oscar Espejo Sagués <xxgreysoulxx@gmail.com>
2017-03-21 18:53:21 +0100

    Practica a casa

--- Win32Project1/ArraysDynamics.cpp
+++ Win32Project1/ArraysDynamics.cpp
@@ -3,40 +3,30 @@
 #include <ctime>
 #include <conio.h>
 
-int x;
-
-int selectordedificultad(int dificultad){
-	if (dificultad==1){
-		x = 1;
-		return x;
-	}
-	else if (dificultad == 2) {
-		x = 2;
-		return x;
-	}
-	else if (dificultad==3) {
-		x = 3;
-		return x;
-	}
-}
-
-class Player;
-class CoinManager;
 
 
-class MAP {
-	char **md;
-	int row = 5*x;
-	int colum = 5*x*2;
+class Fecha{
+public:
+	Fecha(int, int, int);
+	void mostrarfecha();
 
-	md = new char *[row];
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < row; i++) {
-			md[i] = new char[colum];
-			md[i][j] = '.';
-		}
-	}
-
+private:
+	int dia, mes, anio;
 };
 
+Fecha::Fecha(int _dia, int _mes, int _anio){
+	anio = _anio;
+	mes = _mes;
+	dia = _dia;
+}
+
+void Fecha::mostrarfecha() {
+	std::cout << "La fecha es " << dia << ' / ' << mes << ' / ' << anio;
+}
+
+void main() {
+	Fecha Hoy(21, 3, 2017);
+
+	Hoy.mostrarfecha();
+}
 
